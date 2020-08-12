63d537c05a01d9af7b7d704f9d9c5d9b6fc8c3e2
Carlos Uchoa <carlosjuchoa@hotmail.com>
2020-08-12 14:38:06 -0300

    organizando algums arquivos e fazendo exemplos else if

--- controle/if_else.php
+++ controle/if_else.php
@@ -2,10 +2,33 @@
 <?php
 if(false){
     echo "Serei impresso?<br>";
-    echo "Serei impresso novamente?";
-}else{
-    echo "teste ok";
+}
+
+if(true)
+    echo "Verdadeiro<br>";
+else
+    echo "falso<br>";
+    
+
+
+if(false){
+    echo "Verdadeiro - Parte A<br>";
+    echo "Verdadeiro - Parte B<br>";
+} else {
+    echo "Falso - Parte A<br>";
+    echo "Falso - Parte B<br>";
 }
 
 
+
+
+if(false) {
+    echo "Passo A<br>";
+} else if(false) {
+    echo "Passo B<br>";
+}else {
+    echo "Último Passo<br>";
+}
+
+echo "fim<br>";    
 ?>
