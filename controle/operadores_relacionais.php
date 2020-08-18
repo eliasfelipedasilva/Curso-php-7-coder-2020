2b80a9cf56ed276057468062146181f6ddc1f534
Carlos Uchoa <carlosjuchoa@hotmail.com>
2020-08-18 19:43:43 -0300

    modulo novo e aulas novas adicionadas - estruturas de controle

--- controle/operadores_relacionais.php
+++ controle/operadores_relacionais.php
@@ -14,7 +14,7 @@ var_dump( 111 === '111'); //false
 var_dump( 111 != '111'); //false
 var_dump( 111 !== '111'); //true
 
-echo "<p>Relacionais no If/Else</p>";
+echo "<p>Relacionais no If/Else</p><hr>";
 $idade = 28;
 
 if($idade < 18) {
@@ -24,3 +24,28 @@ if($idade < 18) {
 } else {
     echo "Terceira idade = $idade anos";
 }
+
+echo '<p>Spaceship</p><hr>';
+
+var_dump( 500 <=> 3);//positivo se o operando for maior que o segundo
+var_dump( 50 <=> 50);//zero se forem iguais
+var_dump( 50 <=> 80);//negativo se o segundo operando for maior que o primeiro
+
+
+
+echo '<p>Valores pode ser V ou F</p><hr>';
+var_dump(!!5);//para descobrir se o valor é verdadeiro ou falso
+var_dump(!!0);//para descobrir se o valor é verdadeiro ou falso
+var_dump(!!"");//para descobrir se o valor é verdadeiro ou falso
+var_dump(!!" ");//para descobrir se o valor é verdadeiro ou falso
+
+?>
+<style>
+p{
+    margin-bottom:0px;
+}
+
+hr{
+    margin-top:0px;
+}
+</style>
