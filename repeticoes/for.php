b4ecc64d43dc499227669d45799d8865a4b47e0f
UchoaMaster <carlosjuchoa@hotmail.com>
2020-09-17 21:20:56 -0300

    finalizando capitulo de estrutura de controle 2

--- repeticoes/for.php
+++ repeticoes/for.php
@@ -1,47 +1,52 @@
-<div class="titulo">Laço FOR</div>
-<?php 
+<div class="titulo">Laço For</div>
 
-for ($cont=1; $cont <=5 ; $cont++) { 
-    echo "$cont<br>";
+<?php
+for($cont = 1; $cont <= 5; $cont++) {
+    echo "$cont <br>";
 }
-echo "<hr>";
-for (; $cont <=10 ; $cont++) { 
-    echo "$cont<br>";
-}
+
 echo "<hr>";
 
-for(; $cont <=15 ;){
+for(; $cont <= 10; $cont++) {
+    echo "$cont <br>";
+}
+
+echo "<hr>";
+
+for(; $cont <= 15; ) {
     echo "$cont <br>";
     $cont++;
 }
-echo "<hr>";
 
-$array = [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+$array = [
+    1 => 'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado'
+];
 
 print_r($array);
 
-echo '<br>';
-
 echo "<hr>";
-for ($i=0; $i < count($array) ; $i++) { 
+
+for($i = 1; $i <= count($array); $i++) {
     echo "{$array[$i]} <br>";
 }
 
-$matrix =[
-    [
-        'a', 'e', 'i', 'o', 'u'
-    ],
-    [
-        'b', 'c', 'd'
-    ]
+$matrix = [
+    ['a', 'e', 'i', 'o', 'u'],
+    ['b', 'c', 'd']
 ];
 
-for ($i=0; $i < count($matrix); $i++) { 
-    //  echo "{$matrix[$i]}<br>";
-    for($j = 0; $j < count($matrix[$i]); $j++){
+echo "<hr>";
+
+for($i = 0; $i < count($matrix); $i++) {
+    for($j = 0; $j < count($matrix[$i]); $j++) {
         echo "{$matrix[$i][$j]} ";
     }
     echo "<br>";
 }
-
-?>
