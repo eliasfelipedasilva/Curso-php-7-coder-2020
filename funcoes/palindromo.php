6d1335a119a1012bf363883485b3d617e954106c
Carlos Uchoa <carlosjuchoa@hotmail.com>
2020-09-27 10:27:54 -0300

    atividade prática palindromo

--- funcoes/palindromo.php
+++ funcoes/palindromo.php
@@ -6,21 +6,49 @@
 <?php 
 // PHP code to check for Palindrome string in PHP 
 // Using strrev() 
-function Palindrome($string){   
-    if (strrev($string) == $string){   
-        return 1;   
-    } 
-    else{ 
-        return 0; 
-    } 
-}   
+// function Palindrome($string){   
+//     if (strrev($string) == $string){   
+//         return 1;   
+//     } 
+//     else{ 
+//         return 0; 
+//     } 
+// }   
   
 // Driver Code 
-$original = "mateus";  
-if(Palindrome($original)){   
-    echo "A Palavra '$original' é um Palindromo";   
-}  
-else {   
-echo "A Palavra '$original' , não é um Palindromo";   
-} 
+// $original = "dracula";  
+// if(Palindrome($original)){   
+//     echo "A Palavra '$original' é um Palindromo";   
+// }  
+// else {   
+// echo "A Palavra '$original' , não é um Palindromo";   
+// } 
+// function palindromo($palavra) {
+//     $ultimoIndice = strlen($palavra) - 1;
+//     for ($i=0; $i <= $ultimoIndice ; $i++) { 
+//         # code...
+//         if($palavra[$i] !== $palavra[$ultimoIndice - $i]) {
+//             return 'Não é um Palindromo!';
+//         }
+//     }
+//     return 'Sim é um Palindromo';
+// }
+
+// echo palindromo('arara').'<br>';
+// echo palindromo('bola').'<br>';
+// echo palindromo('cabelo').'<br>';
+// echo palindromo('ana').'<br>';
+// echo palindromo('ama').'<br>';
+
+
+function palindromoSimples($palavra) {
+    return $palavra === strrev($palavra) ? 'Sim é um Palindromo!!' : 'Não é um palindromo!';
+
+}
+echo palindromoSimples('arara').'<br>';
+echo palindromoSimples('bola').'<br>';
+echo palindromoSimples('cabelo').'<br>';
+echo palindromoSimples('ana').'<br>';
+echo palindromoSimples('ama').'<br>';
+
 ?> 
