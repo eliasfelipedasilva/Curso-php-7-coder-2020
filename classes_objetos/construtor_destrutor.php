e7bdda4a14e72ca4c2a232f94640523e9367a14c
UchoaMaster <carlosjuchoa@hotmail.com>
2020-11-11 22:14:33 -0300

    finalizando prática de construtor e desconstrutores

--- classes_objetos/construtor_destrutor.php
+++ classes_objetos/construtor_destrutor.php
@@ -1,5 +1,6 @@
 <div class="titulo">Construtor e Destrutor</div>
 
+
 <?php 
 
 class Pessoa {
@@ -7,7 +8,34 @@ class Pessoa {
     public $idade;
     public $cidade;
     public $profissao;
+
+    //pode se usar nomes diferentes, aqui usei para mostrar
+    function __construct($novoNome, $idade, $cidade, $profis) {
+       echo 'Construtor invocado!! <br>';
+        $this->nome =$novoNome;
+        $this->idade = $idade;
+        $this->cidade = $cidade;
+        $this->profissao = $profis;
+    }
+
+    function __destruct() {
+        echo " E morreu o Construtor! <br>";
+    }
+
+    public function apresentar() {
+        echo "{$this->nome}, {$this->idade} anos, Cidade: {$this->cidade},Profissão: {$this->profissao}";
+
+    }
+
 }
+
+$pessoaA = new Pessoa('Carlos Uchôa', 36, 'Brusque-SC', 'Programador<br>');
+$pessoaA->apresentar();
+unset($pessoaA);
+
+$pessoaB = new Pessoa('Erilene Lima', 45, 'Brusque-SC', 'RH<br>');
+$pessoaB->apresentar();
+$pessoaB = null;
 
 
 
