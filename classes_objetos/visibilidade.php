596e9820da8a0034bae237c093073a0d1bfc2e72
Carlos Uchoa <carlosjuchoa@hotmail.com>
2020-11-14 10:07:38 -0300

    aula de visibilidade, prática legal , public, protected, private

---
+++ classes_objetos/visibilidade.php
@@ -0,0 +1,46 @@
+<div class="titulo">Visibilidade</div>
+
+<?php 
+class A {
+    public $publico = 'Público';
+    protected $protegido = 'Protegido';
+    private $privado = 'Privado';
+
+    public function mostrarA() {
+        echo "Class A) Público = {$this->publico}<br>";
+        echo "Class A) Protegido = {$this->protegido}<br>";
+        echo "Class A) Privado = {$this->privado}<br>";
+    }
+    protected function vaiPorHeranca() {
+        echo 'Serei Transmitido por Herança!!<br>';
+    }
+    private function naoMostrar() {
+        echo 'Não vou imprimir!!!<br>';
+    }
+
+}
+
+$a = new A();
+$a->mostrarA();
+// $a->naoMostrar();
+
+class B extends A {
+    public function mostrarB() {
+        echo "Class B) Público = {$this->publico}<br>";
+        echo "Class B) Protegido = {$this->protegido}<br>";
+        echo "class B) Privado = {$this->privado}<br>";
+
+        parent::vaiPorHeranca();
+
+    }
+}
+
+echo '<br>';
+$b = new B();
+$b->mostrarB();
+echo '<br>';
+$b->mostrarA();
+// $b->vaiPorHeranca();
+// $a->naoMostrar();
+
+?>
--- index.php
+++ index.php
@@ -37,6 +37,11 @@
                                 Herança
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=classes_objetos&file=visibilidade">
+                                Visibilidade
+                            </a>
+                        </li>
                     </ul>
                 </div>    
                 <div class="modulo vermelho-escuro">
